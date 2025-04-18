08da4c10d3424a8c1e72868412d06b50410c2d0a
quangminhhh <9a1minhcl@gmail.com>
2025-04-18 16:56:08 +0700

    Install and Config shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
     content: [
       "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}", // Quan trọng: Đảm bảo dòng này đúng
+      "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
       extend: {},
