72cc7c8ca8025ee8fb70ac514dca60892c51c2c6
quangminhhh <9a1minhcl@gmail.com>
2025-04-18 16:33:00 +0700

    Init React with Typescript and TailwindCSS, Folder structures

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center mt-8">Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // Quan trọng: Đảm bảo dòng này đúng
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
